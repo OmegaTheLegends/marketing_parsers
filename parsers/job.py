1472d1fb37f9df41612f2640d87d2e7d8a4deac5
OmegaTheLegends <OmegaTheLegends@gmail.com>
2021-11-16 16:52:40 +0300

    del dns from job

--- parsers/job.py
+++ parsers/job.py
@@ -1,18 +1,11 @@
 import schedule
 import time
 from citilink import main as link
-from dns import dns_main
 from vseinstrumenti import vi_main as instrumenti
 from wb_tag import wb_tags_req as tag_wb
 
 import logging
 logging.basicConfig(level=logging.INFO)
-
-def DNS():
-	try:
-		dns_main.main()
-	except Exception as ex:
-		logging.info(f'Failed DNS parser. {ex}')
 
 def CL():
 	try:
@@ -35,10 +28,8 @@ def WBTAG():
 
 schedule.every().day.at("08:00").do(WBTAG)
 schedule.every().day.at("00:20").do(VI)
-schedule.every().day.at("05:00").do(DNS)
 schedule.every().day.at("00:01").do(CL)
 
 while True:
     schedule.run_pending()
     time.sleep(1)
-
